b548575ccadd2df7429187b7c4382bf279021219
Daniel <scerbinadaniel426@gmail.com>
2024-02-10 17:52:28 +0100

    added rock paper scissors game

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 import discord
 from discord.ext import commands
+import random
+import time
 from utils import *
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.AutoShardedBot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Hai fatto l'accesso come {bot.user}")
+    print(f"We logged in as \"{bot.user}\"")
 
-@bot.command()
-async def password(ctx, Length = 8):
-    Password = GeneratePassword(int(Length))
-    await ctx.send(f"your password:\n**{Password}**")
+
+@bot.command(name = "password", description = "generates a password")
+async def password(ctx, length = 8):
+    password = GeneratePassword(int(length))
+    await ctx.send(f"your password:\n**{password}**")
+
 
 @bot.command()
 async def flip(ctx):
-    CoinResult = flipACoin()
-    await ctx.send(f"the coin landed on...\n**{CoinResult}**")
+    coinResult = FlipACoin()
+    await ctx.send(f"the coin landed on...\n**{coinResult}**")
+
+
+@bot.command()
+async def rps(ctx, choice):
+    await ctx.send("rock")
+    time.sleep(0.4)
+    await ctx.send("paper")
+    time.sleep(0.4)
+    await ctx.send("scissors")
+    time.sleep(0.4)
+    await ctx.send("shoot!!")
+    await ctx.send("------------")
+
+    pcChoices = ["rock", "paper", "scissors"]
+
+    pcChoice = random.choice(pcChoices)
+    result = RockPaperScissorsCalculate(choice, pcChoice)
+    
+    await ctx.send(result)
+
 
 bot.run("YOUR TOKEN")
--- utils.py
+++ utils.py
@@ -1,19 +1,44 @@
 import random
 
 def GeneratePassword(Length):
-    Characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_=+[{"
+    characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_=+[{"
 
-    Password = ""
+    password = ""
 
     for i in range(int(Length)):
-        Password += random.choice(Characters)
+        password += random.choice(characters)
 
-    return Password
+    return password
 
-def flipACoin():
-    Choices = ["head",
+
+def FlipACoin():
+    choices = ["head",
                "tails"]
     
-    Result = random.choice(Choices)
+    result = random.choice(choices)
 
-    return Result
+    return result
+
+
+def RockPaperScissorsGame(userChoice):
+    choices = ["rock",
+               "paper",
+               "scissors"]
+    
+    pcChoice = random.choice(choices)
+
+    if userChoice.lower() == pcChoice:
+        return "It's a tie!!"
+    elif userChoice.lower() == "rock" and pcChoice == "scissors" or userChoice.lower() == "scissors" and pcChoice == "paper" or userChoice.lower() == "paper" and pcChoice == "rock":
+        return "**You win!!!!**\nPC lost!"
+    else:
+        return "You lost!!\n**PC wins!!!**"
+    
+    
+def RockPaperScissorsCalculate(userChoice, pcChoice):
+    if userChoice.lower() == pcChoice:
+        return "It's a tie!!"
+    elif userChoice.lower() == "rock" and pcChoice == "scissors" or userChoice.lower() == "scissors" and pcChoice == "paper" or userChoice.lower() == "paper" and pcChoice == "rock":
+        return "**You win!!!!**\nPC lost!"
+    else:
+        return "You lost!!\n**PC wins!!!**"
