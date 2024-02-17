bf454021226c70e050801f3db0d5eaa3de9c9473
Daniel <scerbinadaniel426@gmail.com>
2024-02-17 19:40:45 +0100

    the logs now comprehend the username of the executor too

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -36,23 +36,28 @@ async def on_ready():
 # fun commands
 @bot.tree.command(name = "password", description = "generates a password")
 async def password(interaction: discord.Interaction, length: int = 8):
+    userExecutor = interaction.user.name
     password = GeneratePassword(length)
+
     await interaction.response.send_message(f"your password:\n**{password}**\n\n||THIS MESSAGE WILL BE DELETED IN 30 SECONDS AFTER BEING SENT||", ephemeral=True, delete_after=30)
-    print("password generated")
+    print(f"{userExecutor} generated a password")
 
 
 @bot.tree.command(name="flip", description="flips a coin")
 async def flip(interaction: discord.Interaction, private: bool = True):
+    userExecutor = interaction.user.name
+
     if private:
         await interaction.response.send_message(f"the coin landed on...\n**{FlipACoin()}**", ephemeral=True, delete_after=30)
-        print("flipped a private coin")
+        print(f"{userExecutor} flipped a private coin")
     else:
         await interaction.response.send_message(f"the coin landed on...\n**{FlipACoin()}**")
-        print("flipped a public coin")
+        print(f"{userExecutor} flipped a public coin")
 
 
 @bot.tree.command(name="rps", description="play a game of rock paper scissors with the bot")
 async def rps(interaction: discord.Interaction, choice: str, private: bool = True):
+    userExecutor = interaction.user.name
     pcChoices = ["rock",
                 "paper",
                  "scissors"]
@@ -61,103 +66,123 @@ async def rps(interaction: discord.Interaction, choice: str, private: bool = Tru
 
     if choice != "rock" or choice != "paper" or choice != "scissors":
         await interaction.response.send_message(f"the response must only be rock, paper or scissors")
-        print("user command not accepted, sending error")
+        print(f"{userExecutor} command not accepted, sending error")
     else:
         if private:
             await interaction.response.send_message(f"rock\npaper\nscissors\nshoot!!\n----------------\npc choose: {pcChoice}\nyou choose: {choice}\n----------------\n{result}", ephemeral=True, delete_after=30)
-            print("played a private game of rock paper scissors")
+            print(f"{userExecutor} played a private game of rock paper scissors")
         else:
             await interaction.response.send_message(f"rock\npaper\nscissors\nshoot!!\n----------------\npc choose: {pcChoice}\nyou choose: {choice}\n----------------\n{result}")
-            print("played a public game of rock paper scissors")
+            print(f"{userExecutor} played a public game of rock paper scissors")
 
 
 @bot.tree.command(name="roll", description="roll a dice with said faces")
 async def Roll(interaction: discord.Interaction, faces: int = 6, private: bool = True):
+    userExecutor = interaction.user.name
+
     if private:
         await interaction.response.send_message(f"the dice landed on a **{RollADice(faces)}**", ephemeral=True, delete_after=30)
-        print("rolled a private dice")
+        print(f"{userExecutor} rolled a private dice")
     else:
         await interaction.response.send_message(f"the dice landed on a {RollADice(faces)}")
-        print("rolled a public dice")
+        print(f"{userExecutor} rolled a public dice")
 
 
 @bot.tree.command(name="8ball", description="shake the magic 8 ball")
 async def EightBall(interaction: discord.Interaction,question: str, private: bool = True):
+    userExecutor = interaction.user.name
+
     if private:
         await interaction.response.send_message(ShakeEightBall(question), ephemeral=True, delete_after=30)
-        print("shaked the eight ball privately")
+        print(f"{userExecutor} shaked the eight ball privately")
     else:
         await interaction.response.send_message(ShakeEightBall(question))
-        print("shaked the eight ball privately")
+        print(f"{userExecutor} shaked the eight ball publicly")
 
 
 @bot.tree.command(name="numberguess", description="play a game where you have to guess a number the bot will output")
 async def NumberGuess(interaction: discord.Interaction, guess: int, max: int = 10, private: bool = True):
+    userExecutor = interaction.user.name
+
     if private:
         await interaction.response.send_message(GuessTheNumber(guess, max), ephemeral=True, delete_after=30)
-        print("played a private game of guess the number")
+        print(f"{userExecutor} played a private game of guess the number")
     else:
         await interaction.response.send_message(GuessTheNumber(guess, max))
-        print("played a public game of guess the number")
+        print(f"{userExecutor} played a public game of guess the number")
 
 
 # generate random images of animals with an api
 @bot.tree.command(name="duck", description="get a random image of a duck")
 async def GetRandomDuckImage(interaction: discord.Interaction):
+    userExecutor = interaction.user.name
+
     await interaction.response.send_message(GetRandomDuckImageUrl())
-    print("send a random image of a duck")
+    print(f"{userExecutor} sent a random image of a duck")
 
 
 @bot.tree.command(name="dog", description="get a random image of a dog")
 async def GetRandomDuckImage(interaction: discord.Interaction):
+    userExecutor = interaction.user.name
+
     await interaction.response.send_message(GetRandomDogImageUrl())
-    print("sent a random image of a dog")
+    print(f"{userExecutor} sent a random image of a dog")
 
 
 @bot.tree.command(name="fox", description="get a random image of a fox")
 async def GetRandomDuckImage(interaction: discord.Interaction):
+    userExecutor = interaction.user.name
+
     await interaction.response.send_message(GetRandomFoxImageUrl())
-    print("sent a random image of a fox")
+    print(f"{userExecutor} sent a random image of a fox")
 
 
 @bot.tree.command(name="cat", description="get a random image of a cat")
 async def GetCatImage(interaction: discord.Interaction):
+    userExecutor = interaction.user.name
+
     await interaction.response.send_message(GetRandomCatImageUrl())
-    print("sent a random image of a cat")
+    print(f"{userExecutor} sent a random image of a cat")
 
 
 @bot.tree.command(name="dadjoke", description="get a random dad joke")
 async def RandomDadJoke(interaction: discord.Interaction, private: bool = False):
+    userExecutor = interaction.user.name
     joke = GetRandomDadJoke()
+
     if private:
         await interaction.response.send_message(joke, ephemeral=True)
-        print("made a private dad joke")
+        print(f"{userExecutor} made a private dad joke")
     else:
         await interaction.response.send_message(joke)
-        print("made a public dad joke")
+        print(f"{userExecutor} made a public dad joke")
 
 
 @bot.tree.command(name="reverse", description="reverses a given word or phrase")
 async def WordReverse(interaction: discord.Interaction, word: str, private: bool = True):
+    userExecutor = interaction.user.name
+
     if private:
         await interaction.response.send_message(f"your word: {word}\nreversed word: {ReverseWord(word)}", ephemeral=True, delete_after=30)
-        print("reversed a word privately")
+        print(f"{userExecutor} reversed a word privately")
     else:
         await interaction.response.send_message(f"your word: {word}\nreversed word: {ReverseWord(word)}")
-        print("reversed a word publicly")
+        print(f"{userExecutor} reversed a word publicly")
 
 
 
 # info commands
 @bot.tree.command(name="help", description="get some info about the commands")
 async def GetInfo(interaction: discord.Interaction):
+    userExecutor = interaction.user.name
     await interaction.response.send_message(botInfo, ephemeral=True, delete_after=30)
-    print("info sent to a user")
+    print(f"info sent to {userExecutor}")
 
 
 @bot.tree.command(name="source", description="get the source code of the bot")
 async def GetSource(interaction: discord.Interaction):
+    userExecutor = interaction.user.name
     await interaction.response.send_message("github repository for the source code:\nhttps://github.com/Susser47/Discord-Bot", ephemeral=True, delete_after=30)
-    print("github repo link send to user")
+    print(f"github repo link send {userExecutor}")
 
 bot.run(BotToken.token)
